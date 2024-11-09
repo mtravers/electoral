4409b2f8219bb51d7fd6977266c06c050b51a551
Mike Travers <mt@hyperphor.com>
2024-11-08 20:51:23 -0800

    actually got the per-county display

--- electoral.clj
+++ electoral.clj
@@ -3,18 +3,42 @@
             [clojure.string :as str]
             [org.candelbio.multitool.core :as u]
             [org.candelbio.multitool.cljcore :as ju]
+            [voracious.formats.csv :as csv]
             ))
 
 (def spec
   {:$schema "https://vega.github.io/schema/vega-lite/v5.json",
-   :width 500,
-   :height 300,
+   :width 750,
+   :height 450,
    :data {:url "data/us-10m.json", :format {:type "topojson", :feature "counties"}},
    :transform
-   [{:lookup "id", :from {:data {:url "data/unemployment.tsv"}, :key "id", :fields ["rate"]}}],
+   [#_ {:filter "datum.year == '2020' && datum.party == 'DEMOCRAT'"}
+    {:lookup "id", :from {:data {:url "data/counties2020.csv"}, :key "county_fips", :fields ["candidatevotes" "totalvotes"]}}
+    {:calculate  "(datum.candidatevotes / datum.totalvotes) - 0.5" :as "demf"}
+    ],
    :projection {:type "albersUsa"},
-   :mark "geoshape",
-   :encoding {:color {:field "rate", :type "quantitative" :scale {:scheme "redblue"}}}})
+   :mark {:type "geoshape", :tooltip {:content "data"}}
+   :encoding {:color {:field "demf"
+                      :type "quantitative"
+                      :scale {:scheme "redblue"}}
+              }})
+
+(def raw-data (csv/read-csv-file-maps "/opt/mt/repos/electoral/data/countypres_2000-2020.csv"))
+
+(defn year-data
+  [year]
+  (as-> raw-data d
+    (filter #(= year (:year %)) d)
+    (filter #(= "DEMOCRAT" (:party %)) d)
+    (group-by :county_fips d)
+    (vals d)
+    (map (fn [group] (assoc (first group) :candidatevotes (reduce + (map :candidatevotes group)))) d)
+    (map #(select-keys % [:county_fips :totalvotes :candidatevotes]) d)))
+
+(csv/write-csv-file-maps "/opt/mt/repos/electoral/data/counties2020.csv" (year-data 2020))
+
+
+
 
 (defn expand-template
   "Template is a string containing {foo} elements, which get replaced by corresponding values from bindings. See tests for examples."
